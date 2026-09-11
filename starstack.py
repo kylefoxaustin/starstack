aa66cf7493929e454350b61a0314fa458b82e4c0
Kyle Fox <12345678+kylefoxaustin@users.noreply.github.com>
2026-09-11 11:52:38 -0500

    Version 0.2.4: scopepull archives, --pull, already-stacked rule, gallery

--- starstack.py
+++ starstack.py
@@ -27,7 +27,7 @@ from dataclasses import dataclass, field
 import numpy as np
 import warnings
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 warnings.filterwarnings("ignore")   # astropy is chatty about slightly-off FITS headers
 
